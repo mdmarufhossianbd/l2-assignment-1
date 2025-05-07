60347a626611ddcdb82a75b23691b6a9bbb5d6ef
Md Maruf Hossain <mdmarufh974@gmail.com>
2025-05-08 00:56:36 +0600

    Problem One Solved

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log('TS ready for work');
+function formatString(input: string, toUpper?: boolean): string {
+    if (toUpper === undefined || toUpper === true) {
+        return input.toUpperCase();
+    } else {
+        return input.toLowerCase()
+    }
+}
+
+// console.log(formatString("hello world"));
+// console.log(formatString("hello world", true));
+// console.log(formatString("hello world", false));
+
