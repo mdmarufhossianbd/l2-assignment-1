c14ff76412b03414eb582522c024a0a550a3739d
Md Maruf Hossain <mdmarufh974@gmail.com>
2025-05-08 01:29:59 +0600

    problem 4 and 5 solved

--- index.ts
+++ index.ts
@@ -34,3 +34,44 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
 
 // console.log(concatenateArrays(["a", "b"], ["c"]));
 // console.log(concatenateArrays([1, 2], [3, 4], [5]));
+class Vahicle {
+    private make: string;
+    private year: number;
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo(): string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+
+class Car extends Vahicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model
+    }
+
+    getModel(): string {
+        return `Model: ${this.model}`;
+    }
+}
+
+const myCar = new Car("Toyota", 2020, "Corolla");
+// console.log(myCar.getInfo());
+// console.log(myCar.getModel());
+
+function processValue(value: string | number): number {
+    if (typeof value === "string") {
+        return value.length
+    } else {
+        return value * 2
+    }
+}
+
+// console.log(processValue("hello_world"));
+// console.log(processValue(10));
